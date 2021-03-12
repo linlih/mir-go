67e989ee140c6320914878946236d69077366d46
zixian555 <406964409@qq.com>
2021-03-12 03:27:26 -0800

    update:fib

--- daemon/table/FIB.go
+++ daemon/table/FIB.go
@@ -24,4 +24,87 @@ func CreateFIB() *FIB {
 	return f
 }
 
+func (f *FIB) FindLongestPrefixMatch(identifier *component.Identifier) *FIBEntry {
+	var PrefixList []string
+	for _, v := range identifier.GetComponents() {
+		PrefixList = append(PrefixList, v.ToString())
+	}
+	if table, ok := f.lpm.FindLongestPrefixMatch(PrefixList); ok {
+		return table.(*FIBEntry)
+	}
+	// 匹配失败返回空
+	return nil
+}
 
+func (f *FIB) FindExactMatch(identifier *component.Identifier) *FIBEntry {
+	var PrefixList []string
+	for _, v := range identifier.GetComponents() {
+		PrefixList = append(PrefixList, v.ToString())
+	}
+	if table, ok := f.lpm.FindExactMatch(PrefixList); ok {
+		return table.(*FIBEntry)
+	}
+	// 匹配失败返回空
+	return nil
+}
+
+func (f *FIB) AddOrUpdate(identifier *component.Identifier, logicFaceId uint64, cost uint64) *FIBEntry {
+	var PrefixList []string
+	for _, v := range identifier.GetComponents() {
+		PrefixList = append(PrefixList, v.ToString())
+	}
+	//两种模式 模式一 val == nil func not nil 用处理函数设置val 模式二 val not nil func == nil 直接设置val
+	val := f.lpm.AddOrUpdate(PrefixList, nil, func(val interface{}) interface{} {
+		// not ok 那么val == nil 存入标识
+		if _, ok := (val).(*FIBEntry); !ok {
+			fibEntry := CreateFIBEntry()
+			val = fibEntry
+		}
+		entry := (val).(*FIBEntry)
+		entry.Identifier = identifier
+		entry.NextHopList[logicFaceId] = NextHop{LogicFaceId: logicFaceId, Cost: cost}
+		return entry
+	})
+	return val.(*FIBEntry)
+}
+
+func (f *FIB) EraseByIdentifier(identifier *component.Identifier) error {
+	var PrefixList []string
+	for _, v := range identifier.GetComponents() {
+		PrefixList = append(PrefixList, v.ToString())
+	}
+	return f.lpm.Delete(PrefixList)
+}
+
+func (f *FIB) EraseByFIBEntry(fibEntry *FIBEntry) error {
+	var PrefixList []string
+	for _, v := range fibEntry.Identifier.GetComponents() {
+		PrefixList = append(PrefixList, v.ToString())
+	}
+	return f.lpm.Delete(PrefixList)
+}
+
+func (f *FIB) RemoveNextHopByFace(logicFaceId uint64) uint64 {
+	return f.lpm.TraverseFunc(func(val interface{}) uint64 {
+		if v, ok := val.(*FIBEntry); ok {
+			if v.HasNextHop(logicFaceId) {
+				v.RemoveNextHop(logicFaceId)
+				return 1
+			}
+		} else {
+			fmt.Println("FIBEntry transform fail")
+		}
+		return 0
+	})
+}
+
+func (f *FIB) Size() uint64 {
+	return f.lpm.TraverseFunc(func(val interface{}) uint64 {
+		if _, ok := val.(*FIBEntry); ok {
+			return 1
+		} else {
+			fmt.Println("FIBEntry transform fail")
+		}
+		return 0
+	})
+}
