76ddf4f3b5e7cc602e8c7df2f70036a36b3ec3a1
zixian555 <406964409@qq.com>
2021-03-13 01:02:36 -0800

    add:lpm /**

--- daemon/table/Lpm.go
+++ daemon/table/Lpm.go
@@ -13,24 +13,54 @@ import (
 	"sync"
 )
 
+//
+// 最长前缀树的表示
+// @Description:
+//	1.node的表示是一个递归的表示，在最外层公开声明一个结构体屏蔽内部实现细节
+//
 type LpmMatcher struct {
 	node
 }
 
+
+//
+// 最长前缀树的节点
+//
+// @Description:
+//	1.最长前缀树中包括本节点的val值，一个子节点列表，和一个用于保护本地val修改的锁
+//	2.map的key是子节点对应的前缀(string)，value是node
+//	3.val是interface{}的接口实现，调用获取的时候需要手动匹配真实类型
+//
 type node struct {
 	val   interface{}
 	table *sync.Map
 	lock  *sync.RWMutex
 }
 
+//temp,not uesed
 type nodeAndLock struct {
 	node *node
 	lock *sync.RWMutex
 }
 
+
+//
+// 初始化,主要是避免每次节点递归的时候都需要判断是否存在lock
+//
+// @Description:
+// @return
+//
 func (n *node) Create() {
 	n.lock = &sync.RWMutex{}
 }
+
+
+//
+// 判断节点为空
+//
+// @Description:节点为空的条件有两个：本节点的val是空的，同时子节点的列表也是空的
+// @return bool
+//
 
 func (n *node) empty() bool {
 	if n.val != nil {
@@ -49,12 +79,31 @@ func (n *node) empty() bool {
 	return count == 0
 }
 
+
+//
+// 用于返回，避免val重复的为空判断
+//
+// @Description:bool为空表示该节点没有val值
+// @return (interface{}, bool)
+//
+
 func deref(val interface{}) (interface{}, bool) {
 	if val == nil {
 		return nil, false
 	}
 	return val, true
 }
+
+//
+// 最长前缀匹配
+//
+// @Description:
+//   1.逐层去查找查找key数组的第一级是否存在于自己的子节点列表中，直到key值长度是0，表面该节点是查找到的精确匹配值
+//   2.如果在递归查找过程中，某个key不存在于该节点的子节点列表中，那么该节点就是最长前缀匹配值
+//   3.需要注意的是，在查找路径的每个路径都会加一个读锁
+//   4.返回值的bool表示该节点对应的val值是否真实存在
+// @return (interface{}, bool)
+//
 
 func (n *node) FindLongestPrefixMatch(key []string) (interface{}, bool) {
 	n.lock.RLock()
@@ -79,6 +128,17 @@ func (n *node) FindLongestPrefixMatch(key []string) (interface{}, bool) {
 	return val, found
 }
 
+
+//
+// 精确匹配
+//
+// @Description:
+//   1.逐层去查找查找key数组的第一级是否存在于自己的子节点列表中，直到key值长度是0，表面该节点是查找到的精确匹配值
+//   2.需要注意的是，在查找路径的每个路径都会加一个读锁
+//   3.返回值的bool表示该节点对应的val值是否真实存在
+// @return (interface{}, bool)
+//
+
 func (n *node) FindExactMatch(key []string) (interface{}, bool) {
 	n.lock.RLock()
 	defer n.lock.RUnlock()
@@ -102,6 +162,18 @@ func (n *node) FindExactMatch(key []string) (interface{}, bool) {
 	return val, found
 }
 
+
+//
+// 添加或者更新
+//
+// @Description:
+//   1.逐层去查找查找key数组的第一级是否存在于自己的子节点列表中，直到key值长度是0，表面该节点是查找到的精确匹配值,那么就更新该节点的值
+//   2.如果在某个节点发现该key不存在于子节点列表中，则新建一个节点并加入列表，以此递归到key长度为0,并设置该值
+//   3.val值可以为空，回调函数f也可以为空
+//   4.在回调函数中通常进行val项的更新
+//   5.最后一个节点的修改前需要获取该节点的写锁
+// @return (interface{}, bool)
+//
 
 func (n *node) AddOrUpdate(key []string, val interface{}, f func(val interface{}) interface{}) interface{} {
 	// 写锁
