b3eb68e6cee6d64b7d63ecb18bc962a38c675799
SunnyQjm <qjm253@pku.edu.cn>
2021-04-02 10:16:56 +0800

    add: 添加基本的 ini 格式的配置文件

--- daemon/main.go
+++ daemon/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"mir-go/daemon/fw"
+	"mir-go/daemon/plugin"
+)
+
 func main() {
+	InitForwarder()
+}
+
+func InitForwarder() {
+	// 初始化插件管理器
+	pluginManager := new(plugin.GlobalPluginManager)
+	// TODO: 在这边注册插件
+	//pluginManager.RegisterPlugin()
+
+	// BlockQueue
+	// TODO: 读取配置文件设置包队列大小
+	packetQueue := fw.CreateBlockQueue(100)
+
+	// 初始化转发器
+	forwarder := new(fw.Forwarder)
+	forwarder.Init(pluginManager, packetQueue)
 
 }
