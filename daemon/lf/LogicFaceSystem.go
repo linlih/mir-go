e504423b972131b9f44cf131f7ace0e797c0541f
misiyu <1187239259@qq.com>
2021-04-19 08:48:37 +0800

    fix: LogicFaceSystem.go delete face loginfo

--- daemon/lf/LogicFaceSystem.go
+++ daemon/lf/LogicFaceSystem.go
@@ -100,6 +100,7 @@ func (l *LogicFaceSystem) doFaceClean() {
 	curTime := getTimestampMS()
 	for k, v := range l.logicFaceTable.mLogicFaceTable {
 		if v.state == false {
+			common2.LogInfo("remove logicface id = ", v.LogicFaceId)
 			l.destroyFace(k, v)
 		} else if v.expireTime < curTime { // logicFace已经超时
 			v.Shutdown()        // 调用shutdown关闭logicFace
